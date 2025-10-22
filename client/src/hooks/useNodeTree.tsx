b3a3c7be0d4c8a3d994315254f22d97401d79bcd
Hadriel Benjo <hdriel@gmail.com>
2025-10-22 20:50:59 +0300

    update client

--- client/src/hooks/useNodeTree.tsx
+++ client/src/hooks/useNodeTree.tsx
@@ -68,11 +68,10 @@ const findNodeById = (node: TreeNodeItem | null, nodeId: string): TreeNodeItem |
 
 interface UseNodeTreeProps {
     refreshTrigger: number;
-    openDeleteDialog?: (node: TreeNodeItem) => void;
     onFolderSelect: (path: string) => void;
 }
 
-export const useNodeTree = ({ refreshTrigger, openDeleteDialog, onFolderSelect }: UseNodeTreeProps) => {
+export const useNodeTree = ({ refreshTrigger, onFolderSelect }: UseNodeTreeProps) => {
     const [treeData, setTreeData] = useState<TreeNodeItem | null>(null);
     const [expanded, setExpanded] = useState<string[]>([]);
     const [, setSelectedIds] = useState<string[]>(['root']);
@@ -82,63 +81,10 @@ export const useNodeTree = ({ refreshTrigger, openDeleteDialog, onFolderSelect }
         loadRootFiles();
     }, [refreshTrigger]);
 
-    const buildNodeLabel = (
-        node: AwsTreeItem,
-        nodeId: string,
-        nodePath: string,
-        parentId: string,
-        { paddingDeleteAction = '-5px' }: { paddingDeleteAction?: string } = {}
-    ) => {
-        const isDirectory = node.type === 'directory';
-
-        const label = (
-            <Box className="item-icon" style={{ display: 'flex', alignItems: 'center', gap: '8px', width: '100%' }}>
-                {isDirectory ? null : <SVGIcon muiIconName={getFileIcon(isDirectory ? '' : node.name)} size={'18px'} />}
-                <Typography className="item-name">
-                    {node.name} {node.children?.length ? `(${node.children.length})` : ''}
-                </Typography>
-                <Box sx={{ marginInlineStart: 'auto' }}>
-                    {!isDirectory && node.size !== undefined && (
-                        <Typography className="item-size">{formatFileSize(node.size)}</Typography>
-                    )}
-                </Box>
-
-                {node.path && node.path !== '/' && (
-                    <Button
-                        icon={isDirectory ? 'DeleteForever' : 'Delete'}
-                        size="small"
-                        tooltipProps={{
-                            placement: 'top',
-                            title: (
-                                <p style={{ width: 'max-content' }}>
-                                    Delete {node.type}:<br />
-                                    {isDirectory ? nodePath : node.name}
-                                </p>
-                            ),
-                        }}
-                        sx={{ ...(!isDirectory && { marginInlineEnd: paddingDeleteAction }) }}
-                        onClick={() =>
-                            openDeleteDialog?.({
-                                id: nodeId,
-                                path: nodePath,
-                                directory: isDirectory,
-                                name: node.name,
-                                parentId,
-                            } as TreeNodeItem)
-                        }
-                    />
-                )}
-            </Box>
-        ) as unknown as string;
-
-        return label;
-    };
-
     function buildTreeData(root: AwsTreeItem, parentId: string | null = null, level = 0): TreeNodeItem | null {
         if (!root) return null;
 
         const nodeId = !root.path || root.path === '/' ? 'root' : root.path || '/';
-        // const label = buildNodeLabel(root, nodeId, root.path, parentId || 'root', { paddingDeleteAction: '-1px' });
         const isDirectory = root.type === 'directory';
 
         return {
@@ -149,13 +95,7 @@ export const useNodeTree = ({ refreshTrigger, openDeleteDialog, onFolderSelect }
             size: isDirectory ? '' : formatFileSize(root.size),
             directory: isDirectory,
             iconName: getFileIcon(isDirectory ? '' : root.name, isDirectory),
-            // sx: {
-            //     '& .MuiTreeItem-content': { paddingY: '0 !important' },
-            //     ...((root.type === 'file' || !root.name) && {
-            //         '& .MuiTreeItem-label': { marginLeft: '-1px' },
-            //         '& .MuiTreeItem-iconContainer': { display: 'none' },
-            //     }),
-            // },
+
             children: root.children
                 ?.map((node) => buildTreeData(node, nodeId, level + 1))
                 .filter((v) => v) as TreeNodeItem[],
@@ -192,7 +132,6 @@ export const useNodeTree = ({ refreshTrigger, openDeleteDialog, onFolderSelect }
                             currNode.type === 'file' ? currNode.path : `${node.path ?? ''}/${currNode.path}`;
 
                         const currNodeId = currNodePath;
-                        // const label = buildNodeLabel(currNode, nodeId, currNodePath, currNodeId);
                         const isDirectory = currNode.type === 'directory';
 
                         return {
@@ -200,17 +139,10 @@ export const useNodeTree = ({ refreshTrigger, openDeleteDialog, onFolderSelect }
                             parentId: node.path,
                             path: currNodePath,
                             name: currNode.name,
-                            // label,
                             size: isDirectory ? '' : formatFileSize(currNode.size),
                             directory: isDirectory,
                             children: [],
                             iconName: getFileIcon(isDirectory ? '' : node.name, isDirectory),
-                            // sx: {
-                            //     ...((currNode.type === 'file' || !currNode.name) && {
-                            //         '& .MuiTreeItem-label': { marginLeft: '-5px' },
-                            //         '& .MuiTreeItem-iconContainer': { display: 'none' },
-                            //     }),
-                            // },
                         } as TreeNodeItem;
                     });
 
@@ -235,20 +167,7 @@ export const useNodeTree = ({ refreshTrigger, openDeleteDialog, onFolderSelect }
         const updateNodes = (nodes: TreeNodeItem[]): TreeNodeItem[] => {
             return nodes.map((node) => {
                 if (node.id === nodeId) {
-                    const label = buildNodeLabel(
-                        {
-                            children: children as unknown as AwsTreeItem[],
-                            type: node.directory ? 'directory' : 'file',
-                            name: node.name,
-                            path: node.path,
-                            size: 0,
-                        } as AwsTreeItem,
-                        nodeId,
-                        node.path,
-                        'root',
-                        { paddingDeleteAction: '-1px' }
-                    );
-                    return { ...node, label, children };
+                    return { ...node, children };
                 }
                 if (node.children) {
                     return { ...node, children: updateNodes(node.children) };
