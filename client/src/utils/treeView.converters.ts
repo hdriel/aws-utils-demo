0d14daa625d6ad9ba0265520809c392165a5ea3c
Hadriel Benjo <hdriel@gmail.com>
2025-10-25 23:12:22 +0300

    update server

--- client/src/utils/treeView.converters.ts
+++ client/src/utils/treeView.converters.ts
@@ -1,8 +1,8 @@
 import { lighten } from '@mui/material';
 import { ListObjectsOutput, S3ResponseFile } from '../types/aws.ts';
 import { AwsTreeItem, TreeNodeItem } from '../types/ui.ts';
-import { formatFileSize, getFileIcon } from './fileUtils.ts';
-import { randomColorDirectory } from '../utils/random-color.ts';
+import { getFileIcon } from './fileUtils.ts';
+import { randomColorDirectory } from './random-color.ts';
 
 export const buildTreeFromFiles = (result: ListObjectsOutput, basePath: string = ''): AwsTreeItem => {
     const { files, directories } = result;
@@ -43,34 +43,6 @@ export const buildTreeFromFiles = (result: ListObjectsOutput, basePath: string =
         children: !basePath || children.length ? children : ([] as AwsTreeItem[]),
     };
 };
-
-export function buildTreeData(
-    root: AwsTreeItem,
-    parentId: string | null = null,
-    level = 0,
-    color: string = ''
-): TreeNodeItem | null {
-    if (!root) return null;
-
-    const nodeId = root.path || '/';
-    const isDirectory = root.type === 'directory';
-    const directoryColor = color || randomColorDirectory();
-
-    return {
-        id: nodeId,
-        parentId,
-        path: root.path,
-        name: root.name,
-        size: isDirectory ? '' : formatFileSize(root.size),
-        directory: isDirectory,
-        iconName: getFileIcon(isDirectory ? '' : root.name, isDirectory),
-        color: directoryColor,
-        bgColor: directoryColor && lighten(directoryColor, 0.9),
-        children: root.children
-            ?.map((node) => buildTreeData(node, nodeId, level + 1, node.type === 'directory' ? '' : directoryColor))
-            .filter((v) => v) as TreeNodeItem[],
-    };
-}
 
 export const getNewRootTreeItem = () => {
     const directoryColor = randomColorDirectory();
